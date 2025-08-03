8da12279d41db1787f3144f65f8696299a5cf6ab
Tanmay Singh <tanmaysinghx@gmail.com>
2025-08-03 09:47:52 +0530

    Test

--- jenkins/pipelines/build-pipelines/ts-auth-service-1625.groovy
+++ jenkins/pipelines/build-pipelines/ts-auth-service-1625.groovy
@@ -2,13 +2,22 @@ pipeline {
   agent any
 
   environment {
-    ENV_FILE = 'ts-auth-service-1625/.env'
+    SERVICE_DIR = 'ts-auth-service-1625'
+    ENV_FILE = "${SERVICE_DIR}/.env"
   }
 
   stages {
-    stage('Checkout Code') {
+    stage('Checkout Infra') {
       steps {
-        dir('ts-auth-service-1625') {
+        git branch: 'main',
+            url: 'https://github.com/tanmaysinghx/ts-root-project-0001.git',
+            credentialsId: 'github-token'
+      }
+    }
+
+    stage('Checkout Service') {
+      steps {
+        dir("${SERVICE_DIR}") {
           git branch: 'main',
               url: 'https://github.com/tanmaysinghx/ts-auth-service-1625.git',
               credentialsId: 'github-token'
@@ -18,24 +27,24 @@ pipeline {
 
     stage('Setup .env') {
       steps {
-        withCredentials([file(credentialsId: 'ts-auth.env', variable: 'ENV_SECRET')]) {
-          sh 'cp "$ENV_SECRET" "ts-auth-service-1625/.env"'
+        withCredentials([file(credentialsId: 'ts-auth-env', variable: 'ENV_SECRET')]) {
+          sh 'cp $ENV_SECRET $ENV_FILE'
         }
       }
     }
 
     stage('Build Docker Image') {
       steps {
-        dir('ts-auth-service-1625') {
-          sh 'docker-compose build'
+        dir("${SERVICE_DIR}") {
+          sh 'docker compose build' // or use 'docker-compose build' if V1 is installed
         }
       }
     }
 
-    stage('Run or Push') {
+    stage('Run Docker') {
       steps {
-        dir('ts-auth-service-1625') {
-          sh 'docker-compose up -d'
+        dir("${SERVICE_DIR}") {
+          sh 'docker compose up -d'
         }
       }
     }

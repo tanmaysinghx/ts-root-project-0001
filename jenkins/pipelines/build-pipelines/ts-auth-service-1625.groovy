ca7d1cfda8a59d16e44f292d4de041093feb46a3
Tanmay Singh <tanmaysinghx@gmail.com>
2025-08-03 09:52:49 +0530

    Test

--- jenkins/pipelines/build-pipelines/ts-auth-service-1625.groovy
+++ jenkins/pipelines/build-pipelines/ts-auth-service-1625.groovy
@@ -2,12 +2,12 @@ pipeline {
   agent any
 
   environment {
-    SERVICE_DIR = 'ts-auth-service-1625'
-    ENV_FILE = "${SERVICE_DIR}/.env"
+    SERVICE_NAME = 'ts-auth-service-1625'
+    ENV_FILE = "${SERVICE_NAME}/.env"
   }
 
   stages {
-    stage('Checkout Infra') {
+    stage('Checkout Root Project') {
       steps {
         git branch: 'main',
             url: 'https://github.com/tanmaysinghx/ts-root-project-0001.git',
@@ -15,9 +15,9 @@ pipeline {
       }
     }
 
-    stage('Checkout Service') {
+    stage('Checkout Auth Service') {
       steps {
-        dir("${SERVICE_DIR}") {
+        dir("${SERVICE_NAME}") {
           git branch: 'main',
               url: 'https://github.com/tanmaysinghx/ts-auth-service-1625.git',
               credentialsId: 'github-token'
@@ -27,23 +27,28 @@ pipeline {
 
     stage('Setup .env') {
       steps {
-        withCredentials([file(credentialsId: 'ts-auth.env', variable: 'ENV_SECRET')]) {
-          sh 'cp "$ENV_SECRET" "ts-auth-service-1625/.env"'
+        withCredentials([file(credentialsId: 'ts-auth-env', variable: 'ENV_SECRET')]) {
+          // Ensure file is copied inside proper directory with correct permissions
+          sh '''
+            mkdir -p ${SERVICE_NAME}
+            cp "$ENV_SECRET" "${ENV_FILE}"
+            chmod 600 "${ENV_FILE}"
+          '''
         }
       }
     }
 
     stage('Build Docker Image') {
       steps {
-        dir("${SERVICE_DIR}") {
-          sh 'docker compose build' // or use 'docker-compose build' if V1 is installed
+        dir("${SERVICE_NAME}") {
+          sh 'docker compose build'
         }
       }
     }
 
     stage('Run Docker') {
       steps {
-        dir("${SERVICE_DIR}") {
+        dir("${SERVICE_NAME}") {
           sh 'docker compose up -d'
         }
       }
